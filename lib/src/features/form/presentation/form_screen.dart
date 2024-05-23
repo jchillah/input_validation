080434dd06f4a87fc93b88c669a782dedabfec71
jchillah <jchillah@gmail.com>
2024-05-23 10:49:13 +0200

    implemented validateEmail and validatePW

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -45,11 +45,29 @@ class FormScreen extends StatelessWidget {
 
   String? validateEmail(String? input) {
     // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Email darf nicht leer sein';
+    }
+    if (input.length <= 5) {
+      return 'Email muss mindestens 5 Zeichen haben';
+    }
+    if (!input.contains('@')) {
+      return 'Email muss das Zeichen "@" enthalten';
+    }
+    if (!(input.endsWith('.com') || input.endsWith('.de'))) {
+      return 'Email muss mit ".com" oder ".de" enden';
+    }
     return null;
   }
 
   String? validatePw(String? input) {
     // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Passwort darf nicht leer sein';
+    }
+    if (input.length < 6 || input.length > 12) {
+      return 'Passwort muss zwischen 6 und 12 Zeichen lang sein';
+    }
     return null;
   }
 }
